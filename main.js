e66ba1fef1b393e5a3d8ed4f41b6369426cbb92d
luciasupova <73910376+luciasupova@users.noreply.github.com>
2021-03-25 14:54:28 +0100

    notifications page + chatbox

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-$(document).ready(function(){
-          
-    $(window).scroll(function(){
+$(document).ready(function () {
 
-      if($(window).scrollTop()>250){
-        $('header').addClass('orange');
-      }else{
-        $('header').removeClass('orange');
-      }
+  $(window).scroll(function () {
 
-    });
-
-});
-
-$(document).ready(function(){
-          
-  $(window).scroll(function(){
-
-    if($(window).scrollTop()>80){
-      $('#header').addClass('orange');
-    }else{
-      $('#header').removeClass('orange');
+    if ($(window).scrollTop() > 250) {
+      $('header').addClass('orange');
+    } else {
+      $('header').removeClass('orange');
     }
 
   });
 
-});
 
+  $('.chatbox').click(function () {
+    $('#chatbox').toggle();
+
+  });
+
+
+  $('.btn-cancel').click(function () {
+    $('#chatbox').hide();
+
+  });
+
+  $(document).ready(function () {
+
+    $(window).scroll(function () {
+
+      if ($(window).scrollTop() > 80) {
+        $('#header').addClass('orange');
+      } else {
+        $('#header').removeClass('orange');
+      }
+
+    });
+
+  });
+
+});
